0a76d66215887d4398b1348b0b36afec83c8e1f3
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 10:46:17 -0500

    Create reference variable to grid container

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 const pixelGrid = [...Array(16)].map(e => Array(16));
+const gridContainer = document.querySelector('.container');
 
 for(let i = 0; i < 15; i++) {
     for(let j = 0; j < 15; j++) {
-        document.createElement('div');
+        
     }
 }
