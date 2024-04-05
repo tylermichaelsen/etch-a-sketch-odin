0eaa909e6c2f9064ad10adc06a8d4caaf307534a
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 10:51:42 -0500

    Append each grid pixel to grid container

--- javascript.js
+++ javascript.js
@@ -4,5 +4,6 @@ const gridContainer = document.querySelector('.container');
 for(let i = 0; i < 15; i++) {
     for(let j = 0; j < 15; j++) {
         pixelGrid[i][j] = document.createElement('div');
+        gridContainer.appendChild(pixelGrid[i][j]);
     }
 }
