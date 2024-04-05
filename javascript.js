b04ccbbc8cc86fb7edaa264a5e005d5793af60b3
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 11:09:43 -0500

    Create columns div and create class for it

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 const pixelGrid = [...Array(16)].map(e => Array(16));
+const columns = [];
 const gridContainer = document.querySelector('.grid-container');
 
 for(let i = 0; i < 15; i++) {
     for(let j = 0; j < 15; j++) {
+        columns[j] = document.createElement('div')
+        columns[j].classList.add('col')
+
         pixelGrid[i][j] = document.createElement('div');
+        pixelGrid[i][j].classList.add('pixel')
+
         gridContainer.appendChild(pixelGrid[i][j]);
     }
 }
