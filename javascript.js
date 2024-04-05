6385636bd9cf3483588292533f86109897d4c8f2
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 12:57:08 -0500

    Draw default grid until reset

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 
+let hasReset = false;
 
 const defaultPixelGrid = [...Array(16)].map(e => Array(16));
 const columns = [];
@@ -15,24 +16,46 @@ newBtn.addEventListener('click', (event) => {
             defaultPixelGrid[i][j].style.backgroundColor = 'white';
         }
     }
+    hasReset = true;
     let gridSize = prompt('Please enter a single number for new grid size (n*n)');
+    pixelGrid = [...Array(gridSize)].map(e => Array(gridSize));
 })
 
-for(let i = 0; i < 16; i++) {
-    columns[i] = document.createElement('div')
-    columns[i].classList.add('col')
-    for(let j = 0; j < 16; j++) {
-        defaultPixelGrid[i][j] = document.createElement('div');
-        defaultPixelGrid[i][j].classList.add('pixel')
-
-        defaultPixelGrid[i][j].textContent = "yo";
-
-        defaultPixelGrid[i][j].addEventListener('mouseover', (event) => {
-            event.target.style.backgroundColor = 'black';
-        });
-        
-        gridContainer.appendChild(columns[i]);
-        columns[i].appendChild(defaultPixelGrid[i][j]);
+if(!hasReset) {
+    for(let i = 0; i < 16; i++) {
+        columns[i] = document.createElement('div')
+        columns[i].classList.add('col')
+        for(let j = 0; j < 16; j++) {
+            defaultPixelGrid[i][j] = document.createElement('div');
+            defaultPixelGrid[i][j].classList.add('pixel')
+    
+            defaultPixelGrid[i][j].textContent = "yo";
+    
+            defaultPixelGrid[i][j].addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor = 'black';
+            });
+            
+            gridContainer.appendChild(columns[i]);
+            columns[i].appendChild(defaultPixelGrid[i][j]);
+        }
+    }
+} else {
+    for(let i = 0; i < gridSize; i++) {
+        columns[i] = document.createElement('div')
+        columns[i].classList.add('col')
+        for(let j = 0; j < gridSize; j++) {
+            pixelGrid[i][j] = document.createElement('div');
+            pixelGrid[i][j].classList.add('pixel')
+    
+            pixelGrid[i][j].textContent = "yo";
+    
+            pixelGrid[i][j].addEventListener('mouseover', (event) => {
+                event.target.style.backgroundColor = 'black';
+            });
+            
+            gridContainer.appendChild(columns[i]);
+            columns[i].appendChild(pixelGrid[i][j]);
+        }
     }
 }
 
