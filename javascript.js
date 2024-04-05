1968da5ace9241663d12fe7e1d8d88e2a62ca3eb
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 13:31:09 -0500

    Refactor

--- javascript.js
+++ javascript.js
@@ -1,45 +1,34 @@
 const body = document.querySelector('body');
 
 let hasReset = false;
+let gridSize = 16;
 
-const defaultPixelGrid = [...Array(16)].map(e => Array(16));
+const pixelGrid = [];
 const columns = [];
 const gridContainer = document.querySelector('.grid-container');
 
-
+for(let i = 0; i < gridSize; i++) {
+    pixelGrid[i] = [];
+    for(let j = 0; j < gridSize; j++) {
+        pixelGrid[i][j] = j;
+    }
+}
 
 const newBtn = document.querySelector('.reset');
 
 newBtn.addEventListener('click', (event) => {
-    for(let i = 0; i < 16; i++ ) {
-        for(let j = 0; j < 16; j++) {
-            defaultPixelGrid[i][j].style.backgroundColor = 'white';
+    drawSketchPad(+prompt('Please enter a single number for new grid size (n*n)'));
+    for(let i = 0; i < gridSize; i++ ) {
+        for(let j = 0; j < gridSize; j++) {
+            pixelGrid[i][j].style.backgroundColor = 'white';
+            
         }
     }
     hasReset = true;
-    let gridSize = prompt('Please enter a single number for new grid size (n*n)');
-    pixelGrid = [...Array(gridSize)].map(e => Array(gridSize));
+    
 })
 
-if(!hasReset) {
-    for(let i = 0; i < 16; i++) {
-        columns[i] = document.createElement('div')
-        columns[i].classList.add('col')
-        for(let j = 0; j < 16; j++) {
-            defaultPixelGrid[i][j] = document.createElement('div');
-            defaultPixelGrid[i][j].classList.add('pixel')
-    
-            defaultPixelGrid[i][j].textContent = "yo";
-    
-            defaultPixelGrid[i][j].addEventListener('mouseover', (event) => {
-                event.target.style.backgroundColor = 'black';
-            });
-            
-            gridContainer.appendChild(columns[i]);
-            columns[i].appendChild(defaultPixelGrid[i][j]);
-        }
-    }
-} else {
+function drawSketchPad(gridSize) {
     for(let i = 0; i < gridSize; i++) {
         columns[i] = document.createElement('div')
         columns[i].classList.add('col')
