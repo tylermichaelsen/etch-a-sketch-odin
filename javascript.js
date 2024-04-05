94c65afe3e3598b5e6b22a1463a39f289842a8e6
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 12:54:03 -0500

    Clicking new resets the grid

--- javascript.js
+++ javascript.js
@@ -1,34 +1,38 @@
 const body = document.querySelector('body');
 
-const pixelGrid = [...Array(16)].map(e => Array(16));
+
+const defaultPixelGrid = [...Array(16)].map(e => Array(16));
 const columns = [];
 const gridContainer = document.querySelector('.grid-container');
 
-let gridSize = 16;
+
 
 const newBtn = document.querySelector('.reset');
 
 newBtn.addEventListener('click', (event) => {
-    gridSize = prompt('Please enter a single number for new grid size (n*n)');
-    
+    for(let i = 0; i < 16; i++ ) {
+        for(let j = 0; j < 16; j++) {
+            defaultPixelGrid[i][j].style.backgroundColor = 'white';
+        }
+    }
+    let gridSize = prompt('Please enter a single number for new grid size (n*n)');
 })
 
-
-for(let i = 0; i < gridSize; i++) {
+for(let i = 0; i < 16; i++) {
     columns[i] = document.createElement('div')
     columns[i].classList.add('col')
-    for(let j = 0; j < gridSize; j++) {
-        pixelGrid[i][j] = document.createElement('div');
-        pixelGrid[i][j].classList.add('pixel')
+    for(let j = 0; j < 16; j++) {
+        defaultPixelGrid[i][j] = document.createElement('div');
+        defaultPixelGrid[i][j].classList.add('pixel')
 
-        pixelGrid[i][j].textContent = "yo";
+        defaultPixelGrid[i][j].textContent = "yo";
 
-        pixelGrid[i][j].addEventListener('mouseover', (event) => {
+        defaultPixelGrid[i][j].addEventListener('mouseover', (event) => {
             event.target.style.backgroundColor = 'black';
         });
         
         gridContainer.appendChild(columns[i]);
-        columns[i].appendChild(pixelGrid[i][j]);
+        columns[i].appendChild(defaultPixelGrid[i][j]);
     }
 }
 
