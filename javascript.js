6c225f4b5ecdb1da349a0a52f0770e2f6d67ed98
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 10:50:14 -0500

    Add div for each index of pixelGrid

--- javascript.js
+++ javascript.js
@@ -3,6 +3,6 @@ const gridContainer = document.querySelector('.container');
 
 for(let i = 0; i < 15; i++) {
     for(let j = 0; j < 15; j++) {
-        
+        pixelGrid[i][j] = document.createElement('div');
     }
 }
