9d1012b45fa18e94687f789209484b646d7ccd96
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 20:28:58 -0500

    Create pixelGrid array

--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@ const body = document.querySelector('body');
 
 const gridContainer = document.querySelector('.grid-container');
 
+let gridSize = 16;
+
+const pixelGrid = [];
+for(let i = 0; i < gridSize; i++) {
+    pixelGrid[i] = [];
+    for(let j = 0; j < gridSize; j++) {
+        pixelGrid[i][j] = j;
+    }
+}
