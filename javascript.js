16ea1111015c2def1c0595fc56466054e03ccc36
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-04-05 20:40:19 -0500

    Create drawGrid function

--- javascript.js
+++ javascript.js
@@ -11,3 +11,18 @@ for(let i = 0; i < gridSize; i++) {
         pixelGrid[i][j] = j;
     }
 }
+
+function drawGrid(size) {
+    for(let i = 0; i < size; i++) {
+        const columns = [];
+        columns[i] = document.createElement('div');
+        columns[i].classList.add('col');
+        for(let j = 0; j < size; j++) {
+            pixelGrid[i][j] = document.createElement('div');
+            gridContainer.appendChild(columns[i]);
+            columns[i].appendChild(pixelGrid[i][j]);
+        }
+    }
+}
+
+drawGrid(gridSize);
